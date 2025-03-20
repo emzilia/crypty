918a457b6d6b0cf5c85c37c16736a7d56046f2a1
emzilia <em@zilia.dev>
2025-03-20 17:29:30 -0400

    reorganized to fit packaging standards

---
+++ src/crypty/__main__.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    from crypty.cli import app
+    app.process_arguments()
---
+++ src/crypty/cli.py
@@ -0,0 +1,6 @@
+from .crypty import Crypty
+
+app = Crypty()
+
+if __name__ == "__main__":
+    app.process_arguments()
