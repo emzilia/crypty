cb21783e254e68e7982076f492e6606d9b7be363
emzilia <em@zilia.dev>
2025-10-02 00:17:00 -0400

    fixed help readout

--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@ string help =
 @"Usage: crypty [operand] [filename]
 Encrypts files with AES-GCM encryption using a keyfile.
 Directories are archived with zip or tar before encryption.
-	Operands:
-		-e, --encrypt	encrypts filename to filename.enc
-		-d, --decrypt	decrypts filename to filename.dec
-		-h, --hash		provides the SHA3-256 hash of a file";
+  Operands:
+    -e, --encrypt   encrypts filename to filename.enc
+    -d, --decrypt   decrypts filename to filename.dec
+    -h, --hash      provides the SHA3-256 hash of a file";
 
 void CheckSupport(string operand)
 {
