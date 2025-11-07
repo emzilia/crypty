6aa9d167de397b6d4ae8ab40be0da834c7ec2c8f
emzilia <em@zilia.dev>
2025-11-07 01:47:28 -0500

    added password-based encryption via pbkdf2

--- Decrypt.cs
+++ Decrypt.cs
@@ -67,4 +67,70 @@ class Decrypt
 
 		ExtractArchive(fileName);
 	}
+
+	private static (byte[], byte[]) CreatePbkdf2Key(byte[] salt)
+	{
+		Console.WriteLine("Enter file password: ");
+
+		string pwdString = Console.ReadLine();
+
+		int saltSize = 16; 
+
+		byte[] pwd = Encoding.Unicode.GetBytes(pwdString);
+		
+		if (salt.Length == 0)
+		{
+			salt = new byte[saltSize];
+			using RandomNumberGenerator rng = RandomNumberGenerator.Create();
+			rng.GetBytes(salt);
+		}
+
+		byte[] key = Rfc2898DeriveBytes.Pbkdf2(pwd, salt, 100000, HashAlgorithmName.SHA256, 32);
+
+		return (key, salt);
+	}
+
+
+	public static void DecryptPass(string fileName)
+	{
+		int keySize = 32;
+		int saltSize = 16;
+
+		string decryptPath = Path.Combine(Directory.GetCurrentDirectory(), fileName.Replace(".enc", ".dec"));
+
+		string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+		if (!File.Exists(filePath)) {
+			Console.Error.WriteLine("Error: Unable to load file for decryption");
+			Environment.Exit(1);
+		}
+
+		byte[] ciphertext = File.ReadAllBytes(filePath);
+
+		byte[] salt = new byte[saltSize];
+		Array.Copy(ciphertext, 0, salt, 0, saltSize);
+
+		(byte[] key, byte[] salt) kdf = CreatePbkdf2Key(salt);
+
+		byte[] nonce = new byte[AesGcm.NonceByteSizes.MaxSize];
+		Array.Copy(ciphertext, saltSize, nonce, 0, AesGcm.NonceByteSizes.MaxSize);
+
+		byte[] tag = new byte[AesGcm.TagByteSizes.MaxSize];
+		Array.Copy(ciphertext, ciphertext.Length - AesGcm.TagByteSizes.MaxSize, tag, 0, AesGcm.TagByteSizes.MaxSize);
+
+		byte[] cipher = new byte[ciphertext.Length - MaxTagAndNonceSize - saltSize];
+		Array.Copy(ciphertext, AesGcm.NonceByteSizes.MaxSize + saltSize, cipher, 0, cipher.Length);
+
+		using var aes = new AesGcm(kdf.key, AesGcm.TagByteSizes.MaxSize);
+
+		byte[] plaintextBytes = new byte[ciphertext.Length - MaxTagAndNonceSize - saltSize];
+
+		aes.Decrypt(nonce, cipher, tag, plaintextBytes);
+
+		File.WriteAllBytes(decryptPath, plaintextBytes);
+
+		fileName = fileName.Replace (".enc", ".dec");
+
+		ExtractArchive(fileName);
+	}
 }
--- Encrypt.cs
+++ Encrypt.cs
@@ -66,4 +66,58 @@ class Encrypt
 
 		Environment.Exit(0);
 	}
+
+	private static (byte[], byte[]) CreatePbkdf2Key()
+	{
+		Console.WriteLine("Enter file password: ");
+
+		string pwdString = Console.ReadLine();
+
+		int saltSize = 16; 
+
+		byte[] pwd = Encoding.Unicode.GetBytes(pwdString);
+		
+		byte[] salt = new byte[saltSize];
+		using RandomNumberGenerator rng = RandomNumberGenerator.Create();
+		rng.GetBytes(salt);
+
+		byte[] key = Rfc2898DeriveBytes.Pbkdf2(pwd, salt, 100000, HashAlgorithmName.SHA256, 32);
+
+		return (key, salt);
+	}
+
+	public static void EncryptPass(string fileName)
+	{
+		string plaintextPath = CreateArchiveIfDir(fileName);
+		string ciphertextPath = plaintextPath + ".enc";
+
+		(byte[] key, byte[] salt) kdf = CreatePbkdf2Key();
+
+		using var aes = new AesGcm(kdf.key, AesGcm.TagByteSizes.MaxSize);
+
+		byte[] nonce = new byte[AesGcm.NonceByteSizes.MaxSize];
+		RandomNumberGenerator.Fill(nonce);
+
+		byte[] plaintextBytes = File.ReadAllBytes(plaintextPath);
+
+		byte[] ciphertext = new byte[plaintextBytes.Length];
+
+		byte[] tag = new byte[AesGcm.TagByteSizes.MaxSize];
+
+		aes.Encrypt(nonce, plaintextBytes, ciphertext, tag);
+
+		File.WriteAllBytes(ciphertextPath, kdf.salt);
+		File.AppendAllBytes(ciphertextPath, nonce);
+		File.AppendAllBytes(ciphertextPath, ciphertext);
+		File.AppendAllBytes(ciphertextPath, tag);
+
+		if (File.Exists(ciphertextPath) == true) {
+			if (ciphertextPath.Contains(".zip") || ciphertextPath.Contains (".tar")) {
+				if (File.Exists(plaintextPath)) File.Delete(plaintextPath);
+			}
+		}
+
+		Environment.Exit(0);
+	}
+	
 }
